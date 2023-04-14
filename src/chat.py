771932a6ad8ab4164f10516e46eaf272b3768a9e
Ravidu Perera <sheahn20133@gmail.com>
2023-04-14 14:49:38 +0530

    Add changes

--- src/chat.py
+++ src/chat.py
@@ -29,5 +29,5 @@ def chat():
     except Exception as e:
         return jsonify(error=str(e)), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=5001)
