224dd39ce32dcb39c4638b4ea60d3a744382482e
Ravidu Perera <sheahn20133@gmail.com>
2023-04-24 16:46:49 +0530

    update the jenkins

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -2,20 +2,24 @@ import os
 import tempfile
 import pytest
 import json
-from src import chat
+import config
+import sys
+sys.path.append('/src')
+from src.chat import app
 
 
 @pytest.fixture
 def client():
-    db_fd, chat.app.config['DATABASE'] = tempfile.mkstemp()
-    chat.app.config['TESTING'] = True
+    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
 
-    with chat.app.test_client() as client:
-        with chat.app.app_context():
+    with app.test_client() as client:
+        with app.app_context():
             yield client
 
     os.close(db_fd)
-    os.unlink(chat.app.config['DATABASE'])
+    os.unlink(app.config['DATABASE'])
+
 
 
 def test_chat_invalid_prompt(client):
@@ -24,7 +28,7 @@ def test_chat_invalid_prompt(client):
 
 
 def test_chat_valid_prompt(client):
-    response = client.get('/?prompt=Hi')
+    response = client.get('/chat?prompt=Hi')
     assert response.status_code == 200
     assert 'response' in json.loads(response.get_data(as_text=True))
 
@@ -33,7 +37,7 @@ def test_chat_server_error(client, monkeypatch):
     def mock_create(*args, **kwargs):
         raise Exception('OpenAI API error')
 
-    monkeypatch.setattr(chat.openai.Completion, 'create', mock_create)
+    monkeypatch.setattr(app.openai.Completion, 'create', mock_create)
 
     response = client.get('/?prompt=Hello')
     assert response.status_code == 500
